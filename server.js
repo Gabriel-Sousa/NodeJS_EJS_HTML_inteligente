412b269f36dd183f43e3b9ff410ab7d66d29a46e
Gabriel Sousa <bielslima04@gmail.com>
2021-05-17 09:46:16 -0300

    Fim do curso EJS

--- server.js
+++ server.js
@@ -4,7 +4,40 @@ const app = express()
 
 app.set("view engine", "ejs")
 
-app.get("/", (request, response) => {response.render("pages/index")})
+app.get("/", (request, response) => {
+    const items = [
+        {
+            tittle: "D",
+            message: "esenvolver aplicações/serviços de forma fácil"
+        },
+        {
+            tittle: "E",
+            message: "JS usa JavaScript para renderizar HTML"
+        },
+        {
+            tittle: "M",
+            message: "uito fácil de usar"
+        },
+        {
+            tittle: "A",
+            message: "mazing"
+        },
+        {
+            tittle: "I",
+            message: "nstall ejs"
+        },
+        {
+            tittle: "S",
+            message: "intaxe simples"
+        }
+    ]
+
+    const subtitle = "Uma linguagem de modalagem para criação de página HTML utilizando JS"
+    response.render("pages/index", {
+        qualitys: items,
+        subtitulo: subtitle
+    })
+})
 
 app.get("/sobre", (request, response) => {response.render("pages/about")})
 
